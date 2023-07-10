b1a1b0e99b84d3c073dbe24a488be7678b7cd11d
Nikita Agapov <nikiton.89@yandex.ru>
2023-07-11 01:22:34 +0500

    Add Annotations

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Calculate and return the attack damage parameter for each class."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5+randint(3,5)}')
     if char_class == 'mage':
@@ -11,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Calculate and return the blocked damage parameter for each class."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -20,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Calculate and return the parameter of the special ability."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80+25}»')
@@ -32,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Calculate the training results."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Перечисли и опиши классы."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -77,7 +85,8 @@ def choice_char_class() -> str:
                                'персонажа').lower()
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,5 +97,3 @@ def main() -> None:
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
